9365416d0283e99eb598554beda6e41ab1a534fd
rishab <rkjain02@ad.unc.edu>
2022-04-17 21:46:09 -0400

    focus div based on nav button clicked

--- public/main.js
+++ public/main.js
@@ -1,4 +1,56 @@
 // Focus div based on nav button click
+const singleButton = document.getElementById("singlenav")
+singleButton.addEventListener("click", singleDiv)
+
+function singleDiv() {
+    const singlediv = document.getElementById("single")
+    const guessdiv = document.getElementById("guess")
+    const multidiv = document.getElementById("multi")
+    // activated
+    singlediv.classList.remove("hidden")
+    singlediv.classList.add("active")
+    //deactivated
+    guessdiv.classList.add("hidden")
+    guessdiv.classList.remove("active")
+    multidiv.classList.add("hidden")
+    multidiv.classList.remove("active")
+}
+
+const multiButton = document.getElementById("multinav")
+multiButton.addEventListener("click", multiDiv)
+
+function multiDiv() {
+    const singlediv = document.getElementById("single")
+    const guessdiv = document.getElementById("guess")
+    const multidiv = document.getElementById("multi")
+    //activated
+    multidiv.classList.remove("hidden")
+    multidiv.classList.add("active")
+    //deactivated
+    singlediv.classList.add("hidden")
+    singlediv.classList.remove("active")
+    guessdiv.classList.add("hidden")
+    guessdiv.classList.remove("active")
+}
+
+const guessButton = document.getElementById("guessnav")
+guessButton.addEventListener("click", guessDiv)
+
+function guessDiv() {
+    const singlediv = document.getElementById("single")
+    const guessdiv = document.getElementById("guess")
+    const multidiv = document.getElementById("multi")
+    //activated
+    guessdiv.classList.remove("hidden")
+    guessdiv.classList.add("active")
+    //deactivated
+    multidiv.classList.add("hidden")
+    multidiv.classList.remove("active")
+    singlediv.classList.add("hidden")
+    singlediv.classList.remove("active")
+
+}
+
 
 // Flip one coin and show coin image to match result when button clicked
 
